31d7871cc6d52f9e8c29cf2928b4e73845622db8
Pablo Schaffner <pablo@puntorigen.com>
2025-03-15 18:43:58 -0300

    - increased version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="async-easy-model",
-    version="0.2.3",
+    version="0.2.4",
     author="Pablo Schaffner",
     author_email="pablo@puntorigen.com",
     description="A simplified SQLModel-based ORM for async database operations",
